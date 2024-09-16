3a392ca8486b4516977b3b84b8f0f9d2a4dd05c7
daferguerrero <dafer_guerrero@hotmail.com>
2024-09-15 23:34:54 -0500

    Version final para release v1.0.0

--- src/supermarket-city/supermarket-city.module.ts
+++ src/supermarket-city/supermarket-city.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupermarketEntity } from 'src/supermarket/supermarket.entity/supermarket.entity';
 import { SupermarketService } from 'src/supermarket/supermarket.service';
 import { SupermarketCityController } from './supermarket-city.controller';
-
+import { CityEntity } from 'src/city/city.entity/city.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SupermarketEntity])],
+  imports: [TypeOrmModule.forFeature([SupermarketEntity, CityEntity])],
   providers: [SupermarketService],
   controllers: [SupermarketCityController],
 })
--- src/supermarket/supermarket.module.ts
+++ src/supermarket/supermarket.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SupermarketService } from './supermarket.service';
 import { SupermarketController } from './supermarket.controller';
+import { SupermarketEntity } from './supermarket.entity/supermarket.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from 'src/city/city.entity/city.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SupermarketEntity])],
   providers: [SupermarketService],
   controllers: [SupermarketController]
 })
